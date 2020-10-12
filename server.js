978e757d82182398a6146de93232eef4f45fd4a1
Vladimir Zizic <vlladdiimiir@gmail.com>
2020-10-12 01:22:17 -0400

    connection and controller files created

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var handlebars = require('express-handlebars')
 
-var port = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3000;
 
-var app = express();git 
+var app = express();
+
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json()); 
+
+var routes = require('./controllers/burger-controller.js');
+app.use(routes);
+
+app.listen(PORT, function(){
+    console.log('Server listening on: http://localhost: ' + PORT);
+});
+
