eb4a416a8fba06b9a2d21da4ece5dd7c4f3008a6
Vladimir Zizic <vlladdiimiir@gmail.com>
2020-10-29 06:15:41 -0400

    started writing the controller code

---
+++ controllers/burgerController.js
@@ -0,0 +1,7 @@
+var express = require('express');
+
+// IMPORTING THE MODEL TO USE ITS DATABASE FUNCTIONS
+var burger = require('../models/burger.js');
+
+// CREATING ALL OUR ROUTES ------------
+routes
