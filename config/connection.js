23812bd85f4873dda134c1d5fa495facac853f51
Vladimir Zizic <vlladdiimiir@gmail.com>
2020-10-30 23:17:53 -0400

    server and connection files finished

--- config/connection.js
+++ config/connection.js
@@ -1,3 +1,4 @@
+//===CONNECTING TO MYSQL======================================
 var mySQL = require('mysql');
  
 connection = mySQL.createConnection({
@@ -7,6 +8,7 @@ connection = mySQL.createConnection({
     database: 'burgers_db' 
 })
 
+//===MAKING THE CONNECTION======================================
 connection.connect(function(err){
     if(err){
         console.error('error connecting: '+ err.stack);
@@ -15,4 +17,5 @@ connection.connect(function(err){
     console.log('connected as id ' + connection.threadId);
 });
 
+//===EXPORTING CONNECTION FOR THE ORM TO USE=====================
 module.exports = connection;
--- server.js
+++ server.js
@@ -1,19 +1,24 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var handlebars = require('express-handlebars')
+//===SET HANDLEBARS=================================
+var handlebars = require('express-handlebars') 
 
 var PORT = process.env.PORT || 3000;
 
 var app = express();
 
+//===SERVE STATIC CONTENT FOR THE APP FROM THE PUBLIC DIRECTORY=====
 app.use(express.static('public'));
 
+//===PARSE APPLICATION BODY AS JSON====================
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json()); 
 
+//===IMPORTING ROUTES AND GIVING THE SERVER ACCESS TO THEM=====
 var routes = require('./controllers/burger-controller.js');
 app.use(routes);
 
+//===RUNNING THE SERVER SO IT CAN BEGIN LISTENING TO CLIENTS REQUESTS=======
 app.listen(PORT, function(){
     console.log('Server listening on: http://localhost: ' + PORT);
 });
